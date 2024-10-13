7cdca468515f622ffd7ccf0e2ac2fc15f951da6c
mdmithuncse <mdmithuncse@gmail.com>
2024-10-13 16:57:20 +0600

    Implement encapsulation concept

---
+++ encapsulation/OopPrinciple.Encapsulation/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿namespace OopPrinciple.Encapsulation
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private string accountName;
+        private decimal balance;
+
+        private const decimal zero = 0;
+
+        public BankAccount(string accountNumber, string accountName, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = balance;
+        }
+
+        public string GetAccountName()
+        {
+            return accountName;
+        }
+
+        public string GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                ArgumentOutOfRangeException.ThrowIfNegativeOrZero(amount, nameof(amount));
+
+            balance += amount;
+            Console.WriteLine($"Amount deposited successfully. New balance is: { balance }");
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0 || amount > balance)
+                ArgumentOutOfRangeException.ThrowIfLessThan(amount, zero, nameof(amount));
+
+            balance -= amount;
+            Console.WriteLine($"Amount withdrawed scuusessfully. New balance is: {balance}");
+        }
+    }
+}
---
+++ encapsulation/OopPrinciple.Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OopPrinciple.Encapsulation;
+
+Console.WriteLine("Hello, This is an example of encapsulation implement.");
+
+BankAccount bankAccount = new BankAccount("2111856932449", "Alice", 10000);
+
+Console.WriteLine($"Account Number: { bankAccount.GetAccountNumber() }");
+Console.WriteLine($"Account Name: { bankAccount.GetAccountName() }");
+Console.WriteLine($"Account Balance: {bankAccount.GetBalance()}");
+
+bankAccount.Deposit(300);
+bankAccount.Deposit(400);
+bankAccount.Deposit(200);
+
+bankAccount.Withdraw(100);
+bankAccount.Withdraw(150);
